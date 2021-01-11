318ff5d2917f25abf9fe8b22670f224524b22a56
Lily Roberts <lr452@mail.example.com>
2021-01-11 12:14:14 +0000

    vmax and ylim

--- dissic_profiles2.py
+++ dissic_profiles2.py
@@ -4,7 +4,7 @@ import iris.quickplot as qplt
 import matplotlib.pyplot as plt
 from iris.coord_categorisation import *
 
-cube = iris.load_cube('/disk2/lr452/Downloads/DISSIC_FIX/S_OCEAN/S_Ocean.dissic_Omon_GFDL-CM4_historical_r1i1p1f1_gr_199401-201412_depthregrid_landmask.nc','dissic')
+cube = iris.load_cube('/disk2/lr452/Downloads/DISSIC_FIX/S_OCEAN/S_Ocean.dissic_Omon_NorESM2-LM_historical_r1i1p1f1_gr_199401-201412_landmask.nc','dissic')
 
 
 add_month_number(cube, 'time', name='month_number')
@@ -22,7 +22,7 @@ cube4 = cube3.collapsed('time',iris.analysis.MEAN)
 #extracting a geographical region
 west = 40
 east = 120
-south = -60
+south = -65
 north = -40
 temporary_cube = cube4.intersection(longitude = (west, east))
 cube4_region = temporary_cube.intersection(latitude = (south,north))
@@ -33,5 +33,6 @@ cube4_region.coord('longitude').guess_bounds()
 grid_areas = iris.analysis.cartography.area_weights(cube4_region)
 global_average_variable = cube4_region.collapsed(['longitude'],iris.analysis.MEAN,weights=grid_areas)
 
-qplt.pcolormesh(global_average_variable)
+qplt.pcolormesh(global_average_variable,vmin = 2.1, vmax = 2.35)
+plt.ylim(1000,0)
 plt.show()
