a053847055b6f713e28ed573667d7a05ba54738a
Lily Roberts <lr452@mail.example.com>
2021-01-13 14:37:33 +0000

    assigning variables

--- regression_dic.py
+++ regression_dic.py
@@ -7,13 +7,15 @@ from scipy.stats.mstats import *
 
 cube = iris.load_cube('/disk2/lr452/Downloads/DISSIC_FIX/S_OCEAN/S_Ocean.dissic_Omon_MIROC-ES2L_historical_r1i1p1f2_gn_199401-201412_landmask.nc','dissic')
 
-print(cube)
+print(cube.standard_name)
+print(cube.var_name)
+print(cube.shape)
 
 average_across_time = cube.collapsed(['time'],iris.analysis.MEAN)
 
 average_across_depth = average_across_time.collapsed(['ocean sigma over z coordinate'],iris.analysis.MEAN)
 
-variable1 = cube('latitude')
+variable1 = cube[2]
 variable2 = average_across_depth
 
 slope,intercept,r_value,p_value,std_err = linregress(variable1, variable2)
