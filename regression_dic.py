330021a1de930695afb4415cab3a487c2ab71d68
Lily Roberts <lr452@mail.example.com>
2021-01-13 14:55:09 +0000

    variable shape

--- regression_dic.py
+++ regression_dic.py
@@ -7,8 +7,6 @@ from scipy.stats.mstats import *
 
 cube = iris.load_cube('/disk2/lr452/Downloads/DISSIC_FIX/S_OCEAN/S_Ocean.dissic_Omon_MIROC-ES2L_historical_r1i1p1f2_gn_199401-201412_landmask.nc','dissic')
 
-print(cube.standard_name)
-print(cube.var_name)
 print(cube.shape)
 
 average_across_time = cube.collapsed(['time'],iris.analysis.MEAN)
@@ -17,6 +15,9 @@ average_across_depth = average_across_time.collapsed(['ocean sigma over z coordi
 
 variable1 = cube[2]
 variable2 = average_across_depth
+
+print(variable1.shape)
+print(variable2.shape)
 
 slope,intercept,r_value,p_value,std_err = linregress(variable1, variable2)
 show()
